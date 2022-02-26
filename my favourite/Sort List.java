ccd3100f5077a9b80dd286a577dac13323bdb1ac
Shuang Qiu <qiushuang0625@gmail.com>
2022-02-26 14:17:16 +1100

    26/02/2022

--- my favourite/Sort List.java
+++ my favourite/Sort List.java
@@ -1,3 +1,4 @@
+// leetcode 148
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -9,64 +10,52 @@
  * }
  */
 class Solution {
-    ListNode tail = new ListNode();
-    ListNode nextSubList = new ListNode();
-    
-    // bottom up approach
     public ListNode sortList(ListNode head) {
-        if (head == null || head.next == null)
-            return head;
-        int count = getCount(head);
-        ListNode start = head;
-        ListNode dummyHead = new ListNode();
+        if (head == null || head.next == null) return head;
         
-        for (int size=1;size<count;size=size*2) {
-            tail = dummyHead;
-            while (start != null) {
-                if (start.next == null) {
-                    tail.next = start;
-                    break;
-                }
-                ListNode mid = split(start, size);
-                merge(start, mid);
-                start = nextSubList;
-            }
-        }
+        ListNode middle = findMiddle(head);
+        ListNode startLeft = sortList(head);
+        ListNode startRight = sortList(middle);
         
+        return combineList(startLeft, startRight);
     }
     
-    
-    
-    public ListNode merge(ListNode list1, ListNode list2) {
-        ListNode node = new ListNode();
-        ListNode ptr = node;
-        
-        while (list1!=null && list2 != null) {
-            if (list1.val > list2.val) {
-                ptr.next = list2;
-                ptr = ptr.next;
-                list2 = list2.next;
-            } else {
-                ptr.next = list1;
-                ptr = ptr.next;
-                list1 = list1.next;
-            }
-        }
-        
-        if (list1 != null) ptr.next = list1;
-        else if (list2 != null) ptr.next = list2;
-        
-        return node.next;
-    }
-    
-    
-    private int getCount(ListNode head) {
-        int cnt = 0;
+    private ListNode combineList(ListNode left, ListNode right) {
+        ListNode head = new ListNode();
         ListNode ptr = head;
-        while (ptr != null) {
+        
+        while (left != null && right != null) {
+            if (left.val <= right.val) {
+                ptr.next = left;
+                left = left.next;
+            }
+            else {
+                ptr.next = right;
+                right = right.next;
+            }
             ptr = ptr.next;
-            cnt++;
         }
-        return cnt;
+        
+        if (left != null) {
+            ptr.next = left;
+        } else if (right != null) {
+            ptr.next = right;
+        }
+        
+        return head.next;
+    }
+    
+    private ListNode findMiddle(ListNode node) {
+        ListNode slow = node;
+        ListNode fast = node;
+        // 1, 2
+        // 1, 2, 3
+        while (fast != null && fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode middle = slow.next;
+        slow.next = null;
+        return middle;
     }
 }
